02f79fdb7bf0e3e0a17e1c42601cb0cba5ddf22f
Fatih20 <fnri39@protonmail.com>
2021-12-30 14:49:03 +0700

    Fix header body

--- src/components/body/profile.js
+++ src/components/body/profile.js
@@ -2,14 +2,17 @@ import React from "react";
 import styled from "styled-components";
 
 const Main = styled.div`
+    align-items: center;
     display: flex;
+    flex-direction: column;
     justify-content: center;
-    gap: 20px;
+    gap: 10px;
 `;
 
 const Photo = styled.img`
+    clip-path: circle(47.5% at center);
     display: inline-block;
-    clip-path: circle(50% at center);
+    margin: -5% -5%;
     width: ${({size}) => `${size}px`};
     height: ${({size}) => `${size}px`};
 `;
@@ -28,7 +31,7 @@ const Name = styled.h1`
 export default function Profile (){
     return (
         <Main>
-            <Photo src="Pasfoto.png" size={175}/>
+            <Photo src="Pasfoto.png" size={200}/>
                 <AboutContainer>
                     <Name>Fatih Nararya R. I. </Name>
                 </AboutContainer>
