23c01926c294b3e80289108738164d028c929fe4
Fatih20 <fnri39@protonmail.com>
2022-01-02 16:15:39 +0700

    Changes to events layout

--- src/components/body/event.js
+++ src/components/body/event.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 
 import options from "../../content/options";
@@ -10,29 +10,41 @@ const Main = styled.div`
     box-sizing: border-box;
     display: flex;
     flex-direction: column;
-    gap: 2px;
+    gap: 0;
     padding: 20px;
     width: 60%;
 
 `;
 
 const EventSectionContainer = styled.div`
-    border-bottom: solid 1px rgba(255, 255, 255, 0.75);
-    margin-bottom: 7px;
-    padding-bottom: 7px;
+    border-bottom: solid 1px rgba(255, 255, 255, 0.4);
+    margin-bottom: 8px;
+    padding-bottom: 8px;
 `;
 
 const EventTitle = styled.h2`
-    font-size: 18px;
-`; 
+    font-size: 24px;
+    text-align: center;
+`;
 
-const Subtitle = styled.p`
-    font-weight: 500;
+const EventDate = styled.p`
+    text-align: center;
+`;
+
+const EventSubtitle = styled.p`
+    font-weight: 600;
+    margin-bottom: 5px;
 `;
 
 const Summary = styled.p`
     margin: 0;
     padding: 0;
+`;
+
+const SeeWorkContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
 `;
 
 const SeeWork = styled.p`
@@ -41,27 +53,40 @@ const SeeWork = styled.p`
 `;
 
 const WorkContainer = styled.div`
-    display: flex;
-    flex-direction: column;
+    display: ${({show}) => show ? "flex" : "none"};
 `;
 
-export default function Event ({event : {title, subtitle, timeStart, timeEnd, summary, relevantLink }}){
+export default function Event ({event : {title, subtitle, timeStart, timeEnd, summary, relevantLink : {progress, result} }}){
+    const[attachmentShown, setAttachmentShown] = useState("none");
+    
     const time = timeEnd === null || timeEnd === undefined ? `(${timeStart})` : `(${timeStart} — ${timeEnd})`
+
+    function handleChoiceClick(buttonClicked) {
+        if (buttonClicked === attachmentShown){
+            setAttachmentShown("none")
+        } else {
+            setAttachmentShown(buttonClicked)
+        }
+    }
 
     return (
         <Main>
             <EventSectionContainer>
                 <EventTitle dangerouslySetInnerHTML={{ __html: title }}/>
-                <Subtitle dangerouslySetInnerHTML={{ __html: subtitle }}/>
-                <Subtitle>{time}</Subtitle>
+                <EventDate>{time}</EventDate>
             </EventSectionContainer>
             <EventSectionContainer>
+                <EventSubtitle dangerouslySetInnerHTML={{ __html: subtitle }}/>
                 <Summary dangerouslySetInnerHTML={{ __html: summary }} />
             </EventSectionContainer>
-            <WorkContainer>
-                <SeeWork>See the result my work</SeeWork>
-                <SeeWork>See the process of my work</SeeWork>
+            <SeeWorkContainer>
+                <SeeWork onClick={() => handleChoiceClick("result")}>{attachmentShown === "result" ? "Hide" : "Show"} the result my work</SeeWork>
+                <SeeWork onClick={() => handleChoiceClick("process")}>{attachmentShown === "process" ? "Hide" : "Show"} the process of my work</SeeWork>
+            </SeeWorkContainer>
+            <WorkContainer show={attachmentShown === "none" ? false : true}>
+
             </WorkContainer>
+
         </Main>
     )
 }
