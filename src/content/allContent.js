68db279418d463a7994ac9d89f8682dacf22e5c2
Fatih20 <fnri39@protonmail.com>
2022-01-04 13:44:52 +0700

    Content partially ported

--- src/content/allContent.js
+++ src/content/allContent.js
@@ -37,26 +37,38 @@ const allContent = {
                     },
                     {
                         "title" : "Xournalpp",
-                        "subtitle" : "Desain Ulang <em>Home Page</em>",
+                        "subtitle" : "Desain Ulang Ikon Aksi",
                         "timeStart" : "2021",
                         "timeEnd" : null,
-                        "summary" : "Merombak dan mendesain ulang homepage dari proyek ini menggunakan HTML & CSS, dengan mempertimbangkan kritik dan saran dari pemelihara proyek",
-                        "attachmentType" : "both",
+                        "summary" : "Bekerja sama dengan pemelihara proyek dan desainer UI untuk mendesain ulang semua ikon aksi yang digunakan dalam aplikasi",
+                        "attachmentType" : "progress",
                         "relevantLink" : [
-                            "https://github.com/xournalpp/xournalpp.github.io/pull/49",
-                            "https://xournalpp.github.io/",
+                            "https://github.com/xournalpp/xournalpp/pull/3154",
                         ],
                     },
                     {
-                        "title" : "Xournalpp",
-                        "subtitle" : "Desain Ulang <em>Home Page</em>",
-                        "timeStart" : "2021",
+                        "title" : "elementaryOS/icons",
+                        "subtitle" : "Membuat Ikon Aksi",
+                        "timeStart" : "2020",
                         "timeEnd" : null,
-                        "summary" : "Merombak dan mendesain ulang homepage dari proyek ini menggunakan HTML & CSS, dengan mempertimbangkan kritik dan saran dari pemelihara proyek",
-                        "attachmentType" : "both",
+                        "summary" : "Menambahkan beberapa ikon aksi ke dalam set ikon yang digunakan oleh distro elementaryOS",
+                        "attachmentType" : "progress",
                         "relevantLink" : [
-                            "https://github.com/xournalpp/xournalpp.github.io/pull/49",
-                            "https://xournalpp.github.io/",
+                            "https://github.com/elementary/icons/pull/914",
+                            "https://github.com/elementary/icons/pull/912",
+                            "https://github.com/elementary/icons/pull/909",
+                            "https://github.com/elementary/icons/pull/908",
+                        ],
+                    },
+                    {
+                        "title" : "Hourglass",
+                        "subtitle" : "Membuat Ikon Aplikasi",
+                        "timeStart" : "2020",
+                        "timeEnd" : null,
+                        "summary" : "Membuat ikon aplikasi baru yang sesuai dengan nama aplikasi",
+                        "attachmentType" : "progress",
+                        "relevantLink" : [
+                            "https://github.com/sgpthomas/hourglass/pull/80",
                         ],
                     }
                 ]
@@ -66,75 +78,201 @@ const allContent = {
                 "name" : "Proyek Pribadi",
                 "content" : [
                     {
-                        "title" : "",
-                        "subtitle" : "",
-                        "timeStart" : "",
-                        "timeEnd" : "",
-                        "summary" : "",
+                        "title" : "Info Film : React Edition",
+                        "subtitle" : null,
+                        "timeStart" : "November 2020",
+                        "timeEnd" : null,
+                        "summary" : "Situs daftar film yang paling digemari sekarang. Dibangun dengan database The Movie DB dan framework React.js ",
+                        "attachmentType" : "result",
                         "relevantLink" : [
-                            
+                            "https://fatih20.github.io/Info-Film-React-Edition/ ",
                         ],
-                    }
+                    },
+                    {
+                        "title" : "Info Film",
+                        "subtitle" : null,
+                        "timeStart" : "Oktober 2020",
+                        "timeEnd" : null,
+                        "summary" : "Situs daftar film yang sekarang paling diminati. Dibangun dengan database The Movie DB dan JS, CSS, HTML ",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://fatih20.github.io/Info-Film/",
+                        ],
+                    },
+                    {
+                        "title" : "BMI Calculator",
+                        "subtitle" : null,
+                        "timeStart" : "Desember 2020",
+                        "timeEnd" : null,
+                        "summary" : "Situs penghitung body mass index yang menyertakan visualisasi tipe body mass index yang dihitung dan jaraknya dari tipe lain. Dibuat dengan framework React.js dan library Rechart.js",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://fatih20.github.io/Better-BMI-Calculator/ ",
+                        ],
+                    },
+                    {
+                        "title" : "Another Life Tough Life",
+                        "subtitle" : null,
+                        "timeStart" : "September 2020",
+                        "timeEnd" : null,
+                        "summary" : "Bekerjasama dengan seorang penulis untuk membuat sebuah game choose-your-own-adventure yang dibangun dengan menggunakan engine Take a Path",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://fatih20.github.io/Another-Life-Tough-Life/ ",
+                        ],
+                    },
+                    {
+                        "title" : "Take a Path",
+                        "subtitle" : null,
+                        "timeStart" : "September 2020",
+                        "timeEnd" : null,
+                        "summary" : "Sebuah game engine dibuat dengan Javascript yang digunakan untuk membuat game choose-your-own-adventure berbasis web. ",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://github.com/Fatih20/Take-a-Path",
+                        ],
+                    },
                 ]
             },
             {
                 "name" : "Pengalaman Menulis",
                 "content" : [
                     {
-                        "title" : "",
-                        "subtitle" : "",
-                        "timeStart" : "",
-                        "timeEnd" : "",
-                        "summary" : "",
+                        "title" : "The Study Friend",
+                        "subtitle" : "<em>Content Writer</em>",
+                        "timeStart" : "Agustus 2021",
+                        "timeEnd" : "Sekarang",
+                        "summary" : "Sebuah game engine dibuat dengan Javascript yang digunakan untuk membuat game choose-your-own-adventure berbasis web. ",
+                        "attachmentType" : "result",
                         "relevantLink" : [
-                            
+                            "https://www.instagram.com/thestudyfriend/",
                         ],
-                    }
+                    },
+                    {
+                        "title" : "Rangkul Rasa",
+                        "subtitle" : "<em>Content Writer</em>",
+                        "timeStart" : "Agustus 2021",
+                        "timeEnd" : "Sekarang",
+                        "summary" : "Menulis beragam post tentang topik-topik yang relevan bagi remaja bangsa",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://www.instagram.com/thestudyfriend/",
+                        ],
+                    },
+                    {
+                        "title" : "Xournalpp",
+                        "subtitle" : "Menulis <em>Homepage</em>",
+                        "timeStart" : "Agustus 2021",
+                        "timeEnd" : null,
+                        "summary" : "Menulis dan membuat seluruh konten yang ada di homepage Xournalpp untuk mempromosikan software tersebut",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://xournalpp.github.io",
+                        ],
+                    },
+                    {
+                        "title" : "Fireside Review",
+                        "subtitle" : "Kontributor",
+                        "timeStart" : "Oktober 2020",
+                        "timeEnd" : "Sekarang",
+                        "summary" : "Menulis hampir selusin artikel review dari beragam game yang ditulis dalam bahasa Inggris untuk pembaca internasional",
+                        "attachmentType" : "result",
+                        "relevantLink" : [
+                            "https://firesidedotreviews.wordpress.com/author/narahamsyah/",
+                        ],
+                    },
                 ]
             },
             {
                 "name" : "Pengalaman Organisasi",
                 "content" : [
                     {
-                        "title" : "",
-                        "subtitle" : "",
-                        "timeStart" : "",
-                        "timeEnd" : "",
-                        "summary" : "",
+                        "title" : "OSIS SMAN 2 Surabaya",
+                        "subtitle" : "Ketua IPHO",
+                        "timeStart" : "November 2019",
+                        "timeEnd" : "Desember 2020",
+                        "summary" : "Menulis hampir selusin artikel review dari beragam game yang ditulis dalam bahasa Inggris untuk pembaca internasional",
+                        "attachmentType" : "none",
+                        "relevantLink" : [
+
+                        ],
+                    },
+                    {
+                        "title" : "Dewan Akademik TPB 2021",
+                        "subtitle" : "Tutor Akademik",
+                        "timeStart" : "September 2021",
+                        "timeEnd" : "Desember 2021",
+                        "summary" : "Membantu segenap mahasiswa TPB 2021 dalam hal-hal akademik dengan membuat pembahasan soal tutorial dan mengadakan tutor sebaya",
+                        "attachmentType" : "none",
+                        "relevantLink" : [
+
+                        ],
+                    },
+                    {
+                        "title" : "Badan Pengurus Angkatan STEI 2021",
+                        "subtitle" : "Ketua Divisi Akademik",
+                        "timeStart" : "Desember 2021",
+                        "timeEnd" : "Sekarang",
+                        "summary" : "Memimpin divisi akademik dalam menjalankan tugasnya, yakni membantu segenap massa STEI 2021 dalam permasalahan dan urusan akademik.",
+                        "attachmentType" : "none",
                         "relevantLink" : [
                             
                         ],
-                    }
+                    },
                 ]
             },
             {
                 "name" : "Pendidikan",
                 "content" : [
                     {
-                        "title" : "",
-                        "subtitle" : "",
-                        "timeStart" : "",
-                        "timeEnd" : "",
-                        "summary" : "",
+                        "title" : "Institut Teknologi Bandung",
+                        "subtitle" : "Sekolah Teknik Elektro dan Informatika",
+                        "timeStart" : "Agustus 2021",
+                        "timeEnd" : "Sekarang",
+                        "summary" : "Diterima melalui jalur SBMPTN",
+                        "attachmentType" : "none",
                         "relevantLink" : [
                             
                         ],
-                    }
+                    },
+                    {
+                        "title" : "SMAN 2 Surabaya",
+                        "subtitle" : "Jurusan MIPA",
+                        "timeStart" : "Juli 2018",
+                        "timeEnd" : "Mei 2021",
+                        "summary" : "Diterima melalui jalur SBMPTN",
+                        "attachmentType" : "none",
+                        "relevantLink" : [
+                            
+                        ],
+                    },
                 ]
             },
             {
                 "name" : "Kursus dan Pelatihan",
                 "content" : [
                     {
-                        "title" : "",
-                        "subtitle" : "",
-                        "timeStart" : "",
-                        "timeEnd" : "",
-                        "summary" : "",
+                        "title" : "English First",
+                        "subtitle" : "Frontrunner 9-14",
+                        "timeStart" : "Juli 2018",
+                        "timeEnd" : "Maret 2020",
+                        "summary" : "Mempelajari konsep-konsep <em>advanced</em> dalam bahasa Inggris dengan bimbingan <em>native speaker</em>",
+                        "attachmentType" : "none",
                         "relevantLink" : [
                             
                         ],
-                    }
+                    },
+                    {
+                        "title" : "Progate",
+                        "subtitle" : "Web Development Path",
+                        "timeStart" : "Juli 2021",
+                        "timeEnd" : "Agustus 2021",
+                        "summary" : "Mempelajari konsep-konsep <em>advanced</em> dalam bahasa Inggris dengan bimbingan <em>native speaker</em>",
+                        "attachmentType" : "none",
+                        "relevantLink" : [
+                            
+                        ],
+                    },
                 ]
             },
         ],
