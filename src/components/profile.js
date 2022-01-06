e89003bc9e86f2338b96f8aa6790f9e736039535
Fatih20 <fnri39@protonmail.com>
2022-01-06 11:03:32 +0700

    Add some comment.

--- src/components/profile.js
+++ src/components/profile.js
@@ -14,6 +14,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 import { faCaretLeft } from "@fortawesome/free-solid-svg-icons";
 
+// Custom Function
 import { sanitizeSafely } from "../utilities";
 
 const Main = styled.div`
